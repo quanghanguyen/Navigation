54f840c60a5872fab2020033d223c68b7752fa4f
QuangHa <duongthien.mcs@gmail.com>
2022-10-05 09:35:45 +0700

    0510

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Bar Items"
+        addButton()
+        configureItems()
     }
-
-
+    
+    private func addButton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.center = view.center
+        button.backgroundColor = .systemOrange
+        button.setTitle("Go to view 2", for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        navigationController?.navigationBar.tintColor = .label
+        view.addSubview(button)
+    }
+    
+    @objc func didTapButton() {
+        let newVC = UIViewController()
+        newVC.title = "View 2"
+        newVC.view.backgroundColor = .systemGray
+        newVC.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sign Out",
+                                                                  style: .done,
+                                                                  target: self,
+                                                                  action: nil)
+        navigationController?.pushViewController(newVC, animated: true)
+    }
+    
+    private func configureItems() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: nil
+        )
+    }
 }
 
